a24d3b52b01b4cf8c6db567074f1446232e3d5a7
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-07-29 21:00:31 -0500

    feat: init

---
+++ src/main/java/com/isai/app/AppTutorialSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.isai.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTutorialSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTutorialSpringSecurityApplication.class, args);
+    }
+
+}
